3dcabae04ca1091f3f807e6532b8b36298a33793
JHANSIRANI41 <127426371+JHANSIRANI41@users.noreply.github.com>
2025-08-04 02:05:00 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import Authcontext from './context/Authcontext'
-import userContext from './context/Authcontext'
-import UserContext from './context/userContext'
+import Authcontext from './context/Authcontext.jsx'
+import UserContext from './context/Usercontext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
